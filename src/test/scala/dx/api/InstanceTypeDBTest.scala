147c2e06090164b0bac8d1d42248f11e64768b81
jdidion <github@didion.net>
2020-11-12 20:01:10 -0500

    fix test

--- src/test/scala/dx/api/InstanceTypeDBTest.scala
+++ src/test/scala/dx/api/InstanceTypeDBTest.scala
@@ -159,7 +159,8 @@ class InstanceTypeDBTest extends AnyFlatSpec with Matchers {
     val c1 = dbFull.compareByResources("mem1_ssd1_x2", "mem1_ssd1_x8")
     c1 should be < 0
     // instances where some resources are less and some are greater
-    dbFull.compareByResources("mem1_ssd1_x4", "mem3_ssd1_x2") shouldBe None
+    // the first one is greater than the second because it has more CPU
+    dbFull.compareByResources("mem1_ssd1_x4", "mem3_ssd1_x2") shouldBe 1
     // non existant instance
     assertThrows[Exception] {
       dbFull.compareByResources("mem1_ssd2_x2", "ggxx") shouldBe 0
