3a39afa50283c3870132d9fe5f60270839f56d63
jdidion <github@didion.net>
2020-11-25 17:27:59 -0500

    remove unused template

--- compiler/src/main/scala/dx/compiler/ApplicationCompiler.scala
+++ compiler/src/main/scala/dx/compiler/ApplicationCompiler.scala
@@ -28,7 +28,6 @@ object ApplicationCompiler {
   //private val EcrDockerPreambleTemplate = "templates/ecr_docker_preamble.ssp"
   private val DynamicAppletJobTemplate = "templates/dynamic_applet_script.ssp"
   private val StaticAppletJobTemplate = "templates/static_applet_script.ssp"
-  private val EpilogTemplate = "templates/epilog_script.ssp"
   private val WorkflowFragmentTempalate = "templates/workflow_fragment_script.ssp"
   private val CommandTempalate = "templates/workflow_command_script.ssp"
   // keys used in templates
