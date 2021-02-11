bca2f0c1cc1ee0d3393634db7e78b601e3743814
Aleksandra Zalcman <commandlinegirl@users.noreply.github.com>
2021-02-11 06:28:02 -0800

    Update create_dxCompiler_projects.sh

--- scripts/dxCompiler_projects_utils/create_dxCompiler_projects.sh
+++ scripts/dxCompiler_projects_utils/create_dxCompiler_projects.sh
@@ -6,6 +6,7 @@ declare -A PROJECTS
 PROJECTS[dxCompiler]=aws:us-east-1
 PROJECTS[dxCompiler_Berlin]=aws:eu-central-1
 PROJECTS[dxCompiler_Amsterdam]=azure:westeurope
+PROJECTS[dxCompiler_Azure]=azure:westus
 PROJECTS[dxCompiler_Sydney]=aws:ap-southeast-2
 PROJECTS[dxCompiler_London]=aws:eu-west-2
 
