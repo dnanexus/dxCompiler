aeae215d70f0248755efc41df51c82dbffc5c1f3
jdidion <github@didion.net>
2021-03-01 08:42:45 -0500

    add -useManifests for integration tests that contain 'manifest'

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -450,6 +450,8 @@ def build_test(tname, project, folder, version_id, compiler_flags):
                 "-force",
                 "-folder", folder,
                 "-project", project.get_id() ]
+    if desc.source_file.contains("manifest"):
+        cmdline.append("-useManifests")
     cmdline += compiler_flags
     print(" ".join(cmdline))
     oid = subprocess.check_output(cmdline).strip()
