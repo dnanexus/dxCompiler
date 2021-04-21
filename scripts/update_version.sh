ee565009c146832a3c35b54f6e68f4336b1217e8
Riva Nathans <rnathans-cf@dnanexus.com>
2021-04-21 18:38:26 +0200

    Single version of script that should work w/ both macOS and Linux.

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 VERSION=$1
+
 # Update the config files with the newest version
 CONF_FILES=(
     ./executorWdl/src/main/resources/application.conf
@@ -8,6 +10,9 @@ CONF_FILES=(
     ./compiler/src/main/resources/application.conf
     ./executorCommon/src/main/resources/application.conf
 )
+
+# Adapted to work also w/ macOS version of sed, where -i expects extension arg
+# https://stackoverflow.com/questions/19456518/invalid-command-code-despite-escaping-periods-using-sed/19457213#19457213
 for i in ${CONF_FILES[@]}; do
-    sed -i "s/version.*$/version = \"${VERSION}\"/" $i
+    sed -i'' -e "s/version.*$/version = \"${VERSION}\"/" $i
 done
