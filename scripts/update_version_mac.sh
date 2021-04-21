23b95db2a68546b90285b65706832f127a4574b0
Riva Nathans <rnathans-cf@dnanexus.com>
2021-04-21 16:52:03 +0200

    Version of update_version script that works on macOS.

---
+++ scripts/update_version_mac.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Adapted to work with macOS version of sed, where -i expects extension arg
+# https://stackoverflow.com/questions/19456518/invalid-command-code-despite-escaping-periods-using-sed/19457213#19457213
+
+VERSION=$1
+
+# Update the config files with the newest version
+CONF_FILES=(
+    ./executorWdl/src/main/resources/application.conf
+    ./core/src/main/resources/application.conf
+    ./executorCwl/src/main/resources/application.conf
+    ./compiler/src/main/resources/application.conf
+    ./executorCommon/src/main/resources/application.conf
+)
+for i in ${CONF_FILES[@]}; do
+    sed -i '' -e "s/version.*$/version = \"${VERSION}\"/" $i
+done
