3088f882326b2e6d348061e0300ee8160b120dcd
jdidion <github@didion.net>
2021-08-19 16:04:09 -0700

    fix lints

--- scripts/clean_build.sh
+++ scripts/clean_build.sh
@@ -9,18 +9,18 @@ rm -rf applet_resources
 rm dx*.jar
 
 # Clean artifacts from prev. builds, platform
-username=`dx whoami --id`
+username=$(dx whoami --id)
 rc=$?
 if [[ $rc -ne 0 ]]; then
   echo "Could not get username; you are probably not logged in to DNAnexus"
   exit $rc
 fi
-dx rm -r dxCompiler_playground:/builds/$username
-dx rm -r dxCompiler_playground:/builds/user-$username
-dx rm -r dxCompiler_playground:/unit_tests/$username
-dx rm -r dxCompiler_playground:/unit_tests/user-$username
+dx rm -r "dxCompiler_playground:/builds/$username"
+dx rm -r "dxCompiler_playground:/builds/user-$username"
+dx rm -r "dxCompiler_playground:/unit_tests/$username"
+dx rm -r "dxCompiler_playground:/unit_tests/user-$username"
 
 # Run 1 integration test to re-build, upload
-if [[ ! -z "${TEST}"]]; then
-  ./scripts/run_tests.py --test $TEST
+if [[ -z "$TEST" ]]; then
+  ./scripts/run_tests.py --test "$TEST"
 fi
