842cbf5ac8407cf74df4c2a411e73df73b4cef3e
Riva Nathans <rnathans-cf@dnanexus.com>
2021-09-02 13:08:05 +0200

    Test example, unique folder names.

--- test-dxcompiler-in-project/example_project/test/test_wdl.sh
+++ test-dxcompiler-in-project/example_project/test/test_wdl.sh
@@ -4,16 +4,20 @@ set -exo pipefail
 
 DXCOMPILER_JAR=/home/dnanexus/dxCompiler.jar
 
+# Timestamp for unique test run folder names
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
 # Download files needed for compiling workflows
 dx download -r workflow_1
 
 # Set up test output folder for each workflow
-dx mkdir -p test_out/workflow_1
+WF1_DIR=/test_out/workflow_1_$TIMESTAMP
+dx mkdir -p $WF1_DIR
 
 # Compile workflow 1
 exec_1=`java -jar $DXCOMPILER_JAR compile workflow_1/workflow_1.wdl \
     -extras workflow_1/extras.json \
-    -folder /test_out/workflow_1 \
+    -folder $WF1_DIR \
     -force`
 
 # Run workflow 1 if compiled
@@ -21,7 +25,7 @@ exec_1=`java -jar $DXCOMPILER_JAR compile workflow_1/workflow_1.wdl \
 # Ignore reuse so full workflow runs each time, if benchmarking
 if [[ "$exec_1" == "workflow-"* ]]; then
     dx run $exec_1 \
-        --destination /test_out/workflow_1 \
+        --destination $WF1_DIR \
         --detach \
         --ignore-reuse \
         --brief
