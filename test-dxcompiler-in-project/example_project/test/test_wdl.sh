c83f0774bbbd284257295bbf205f5afe6a4dcd3b
Riva Nathans <rnathans-cf@dnanexus.com>
2021-09-02 13:00:30 +0200

    Test applet example, ignore reuse.

--- test-dxcompiler-in-project/example_project/test/test_wdl.sh
+++ test-dxcompiler-in-project/example_project/test/test_wdl.sh
@@ -12,14 +12,17 @@ dx mkdir -p test_out/workflow_1
 
 # Compile workflow 1
 exec_1=`java -jar $DXCOMPILER_JAR compile workflow_1/workflow_1.wdl \
-	-folder /test_out/workflow_1 \
-	-force`
+    -extras workflow_1/extras.json \
+    -folder /test_out/workflow_1 \
+    -force`
 
 # Run workflow 1 if compiled
 # Use detached jobs so workflows fail individually
+# Ignore reuse so full workflow runs each time, if benchmarking
 if [[ "$exec_1" == "workflow-"* ]]; then
     dx run $exec_1 \
         --destination /test_out/workflow_1 \
         --detach \
+        --ignore-reuse \
         --brief
 fi
