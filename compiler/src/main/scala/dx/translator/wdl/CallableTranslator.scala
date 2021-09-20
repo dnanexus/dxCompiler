79a283805911f70379e4e888187af1f030d4257a
jdidion <github@didion.net>
2021-09-20 09:38:53 -0700

    merge develop

--- compiler/src/main/scala/dx/translator/wdl/CallableTranslator.scala
+++ compiler/src/main/scala/dx/translator/wdl/CallableTranslator.scala
@@ -445,7 +445,7 @@ case class CallableTranslator(wdlBundle: WdlBundle,
         // get the input and output closures of the workflow statements -
         // these are the inputs that come from outside the block, and the
         // outputs that are exposed
-        val (statementClosureInputs, outputs) =
+        val (statementClosureInputs, statementClosureOutputs) =
           WdlUtils.getClosureInputsAndOutputs(statements, withField = true)
         // create block inputs for the closure inputs
         val allInputs = WdlBlockInput.create(statementClosureInputs)
