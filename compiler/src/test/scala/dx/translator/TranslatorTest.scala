8f3b17929a311b547a765b29ac0b04fd054dc63a
John Didion <github@didion.net>
2021-09-21 12:04:17 -0400

    cherry pick

--- compiler/src/test/scala/dx/translator/TranslatorTest.scala
+++ compiler/src/test/scala/dx/translator/TranslatorTest.scala
@@ -1750,4 +1750,10 @@ Main.compile(args.toVector) shouldBe a[SuccessfulCompileIR]
       case ExecutableKindWfFragment(Some("wc3-tool.cwl"), Vector(0), _, None) =>
     }
   }
+
+  it should "translate a WDL workflow with dx runtime attributes" in {
+    val path = pathFromBasename("bugs", "dx_runtime_keys.wdl")
+    val args = path.toString :: cFlags
+    Main.compile(args.toVector) shouldBe a[SuccessfulCompileIR]
+  }
 }
