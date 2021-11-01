4052a053ff904b555e1a96e6e996fbb5f56b7073
Marek Hrvol <mhrvol-cf@dnanexus.com>
2021-11-01 16:22:51 +0100

    test

--- compiler/src/main/scala/dx/translator/wdl/CallableTranslator.scala
+++ compiler/src/main/scala/dx/translator/wdl/CallableTranslator.scala
@@ -696,33 +696,33 @@ case class CallableTranslator(wdlBundle: WdlBundle,
       // link together all the stages into a linear workflow
       val (allStageInfo, stageEnv): (Vector[(Stage, Vector[Callable])], CallEnv) =
         subBlocks.zipWithIndex.foldLeft((Vector.empty[(Stage, Vector[Callable])], inputEnv)) {
-//          case ((stages, beforeEnv), (block: WdlBlock, _: Int))
-//              if block.kind == BlockKind.CallDirect =>
-//            logger.warning("block target HERE!")
-//            logger.warning(block.target.toString())
-//            block.target match {
-//              case Some(call: TAT.Call) if call.afters.isEmpty =>
-//                // The block contains exactly one call with no dependencies and with
-//                // no extra variables. Compile directly into a workflow stage.
-//                logger.warning(" callafters HERE")
-//                logger.warning(call.afters.isEmpty.toString())
-//                logger2.trace(s"Translating call ${call.actualName} as stage")
-//                logger2.info(call.toString())
-//                logger2.info(beforeEnv.toString())
-//                logger2.info(locked.toString())
-//                val stage = translateCall(call, beforeEnv, locked)
-//                // Add bindings for the output variables. This allows later calls to refer
-//                // to these results.
-//                val afterEnv = stage.outputs.foldLeft(beforeEnv) {
-//                  case (env, param: Parameter) =>
-//                    val dxName = param.name.pushDecodedNamespace(call.actualName)
-//                    val paramFqn = param.copy(name = dxName)
-//                    env.add(dxName, (paramFqn, StageInputStageLink(stage.dxStage, param)))
-//                }
-//                (stages :+ (stage, Vector.empty[Callable]), afterEnv)
-//              case _ =>
-//                throw new Exception(s"invalid DirectCall block ${block}")
-//            }
+          case ((stages, beforeEnv), (block: WdlBlock, _: Int))
+              if block.kind == BlockKind.CallDirect =>
+            logger.warning("block target HERE!")
+            logger.warning(block.target.toString())
+            block.target match {
+              case Some(call: TAT.Call) if call.afters.isEmpty =>
+                // The block contains exactly one call with no dependencies and with
+                // no extra variables. Compile directly into a workflow stage.
+                logger.warning(" callafters HERE")
+                logger.warning(call.afters.isEmpty.toString())
+                logger2.trace(s"Translating call ${call.actualName} as stage")
+                logger2.info(call.toString())
+                logger2.info(beforeEnv.toString())
+                logger2.info(locked.toString())
+                val stage = translateCall(call, beforeEnv, locked)
+                // Add bindings for the output variables. This allows later calls to refer
+                // to these results.
+                val afterEnv = stage.outputs.foldLeft(beforeEnv) {
+                  case (env, param: Parameter) =>
+                    val dxName = param.name.pushDecodedNamespace(call.actualName)
+                    val paramFqn = param.copy(name = dxName)
+                    env.add(dxName, (paramFqn, StageInputStageLink(stage.dxStage, param)))
+                }
+                (stages :+ (stage, Vector.empty[Callable]), afterEnv)
+              case _ =>
+                throw new Exception(s"invalid DirectCall block ${block}")
+            }
           case ((stages, beforeEnv), (block: WdlBlock, blockNum: Int)) =>
             // The block requires a "fragment-runner" applet to do some runtime evaluation
             // before the target applet/workflow can be launched.
