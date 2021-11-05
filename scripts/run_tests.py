3308205232dc8a2c8064f491872c5e8a3a468a49
Marek Hrvol <mhrvol-cf@dnanexus.com>
2021-11-05 14:58:14 +0100

    test

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -634,7 +634,9 @@ def register_test(dir_path, tname, ext):
 
     # Verify the input file, and add it (if it exists)
     test_input = os.path.join(dir_path, tname + "_input.json")
+    print(f"TEST INPUT HERE {test_input}")
     if os.path.exists(test_input):
+        print("and it does exist...")
         verify_json_file(test_input)
         desc.raw_input.append(test_input)
         desc.dx_input.append(os.path.join(dir_path, tname + "_input.dx.json"))
