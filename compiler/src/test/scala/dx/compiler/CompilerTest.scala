3cdd95809daba745c8a4fd7107d31e10e7d10122
Riva Nathans <rnathans-cf@dnanexus.com>
2021-11-22 14:19:21 +0100

    Fix test -- expect metadata details field.

--- compiler/src/test/scala/dx/compiler/CompilerTest.scala
+++ compiler/src/test/scala/dx/compiler/CompilerTest.scala
@@ -720,9 +720,10 @@ class CompilerTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll {
           case (Constants.SourceCode, JsString(_))         => () // ignore
           case (Constants.ParseOptions, JsObject(_))       => () // ignore
           // old values for sourceCode - can probalby delete these
-          case ("womSourceCode", JsString(_)) => ()
-          case ("wdlSourceCode", JsString(_)) => ()
-          case other                          => throw new Exception(s"Unexpected result ${other}")
+          case ("womSourceCode", JsString(_))                   => ()
+          case ("wdlSourceCode", JsString(_))                   => ()
+          case ("staticInstanceTypeSelection", JsBoolean(true)) => ()
+          case other                                            => throw new Exception(s"Unexpected result ${other}")
         }
       case other => throw new Exception(s"Unexpected result ${other}")
     }
