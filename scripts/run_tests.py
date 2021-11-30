ba20448796d0a510c89d8397799285f1360ae4d0
Yuxin Shi <yshi@dnanexus.com>
2021-11-29 20:56:47 -0800

    Revert "skip failed compilation and continue run"
    This reverts commit 0527bb46a8be81ed6b40632a8c09316eac5bb7a1.

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -473,7 +473,6 @@ cwl_cromwell_tests_list = [
     "cwl_output_json",
     "prefix_for_array",
     "cwl_recursive_link_directories",
-    "cwl_relative_imports_glob_sort",
     "cwl_relative_imports",
     "cwl_disk_resources", # Unknown hint https://www.dnanexus.com/cwl#InputResourceRequirement
     "cwl_inputdir_zero_doesnt_localize", # failed compiled
@@ -1730,10 +1729,7 @@ def compile_tests_to_project(
         print("runnable({}) = {}".format(tname, oid))
     if errors:
         write_failed(errors)
-        cprint(f"failed to compile one or more tests: {','.join(errors)}", "red")
-        # raise RuntimeError(f"failed to compile one or more tests: {','.join(errors)}")
-    if runnable:
-        cprint(f"succeed to compile one or more tests: {','.join(runnable.keys())}", "red")
+        raise RuntimeError(f"failed to compile one or more tests: {','.join(errors)}")
     return runnable
 
 
