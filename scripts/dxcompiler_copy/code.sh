1f493e6dd3d3c0ecfaa1df0cd14634c20188b573
Aleksandra Zalcman <commandlinegirl@users.noreply.github.com>
2022-01-06 19:37:18 +0100

    APPS-985 Cleanup of the asset copying app from a cn reference (#271)

--- scripts/dxcompiler_copy/code.sh
+++ scripts/dxcompiler_copy/code.sh
@@ -5,10 +5,6 @@
 set -e -x -o pipefail
 
 main() {
-    if [[ $DX_JOB_ID == "aws:cn-north-1" ]]; then
-        url=$(echo $url | sed -e "s/dl\.dnanex\.us/dl\.cn\.dnanexus.com:8002/g")
-    fi
-
     fid=$(wget -qO- ${url} | dx upload - --brief --visibility hidden --destination ${DX_PROJECT_CONTEXT_ID}:${folder}/${filename})
 
     dx-jobutil-add-output ofile --class=file $fid
