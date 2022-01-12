5337bfe88491ae27998532b2eb28bc73eafc82c3
Riva Nathans <rnathans-cf@dnanexus.com>
2022-01-12 15:21:39 +0100

    Whitespace.

--- test/multi_user/global_wf_from_wdl_multiply/src/global_wf_from_wdl_multiply.py
+++ test/multi_user/global_wf_from_wdl_multiply/src/global_wf_from_wdl_multiply.py
@@ -4,12 +4,9 @@ import dxpy
 
 @dxpy.entry_point('main')
 def main(multiply_first, multiply_second):
-
     product = multiply_first * multiply_second
-
     output = {}
     output["product"] = product
-
     return output
 
 dxpy.run()
