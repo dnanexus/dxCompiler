df61e221285bc94cced63289603bead987bffc16
Gvaihir <pcf11differentiation@gmail.com>
2022-02-09 09:00:56 -0800

    APPS-1006 Update include worker error trace (feat) JobMetaTest.scala - WIP

--- executorCommon/src/test/scala/dx/executor/JobMetaTest.scala
+++ executorCommon/src/test/scala/dx/executor/JobMetaTest.scala
@@ -1,3 +1,6 @@
 package dx.executor
 
-class JobMetaTest {}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class WorkerJobMetaTest extends AnyFlatSpec with Matchers {}
