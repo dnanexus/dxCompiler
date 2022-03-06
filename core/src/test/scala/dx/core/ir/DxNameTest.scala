b406ca9be8f1ea6c5f03557fa0d18ceea77be837
Yuxin Shi <yshi@dnanexus.com>
2022-03-05 19:13:43 -0800

    update dxname tests

--- core/src/test/scala/dx/core/ir/DxNameTest.scala
+++ core/src/test/scala/dx/core/ir/DxNameTest.scala
@@ -29,26 +29,32 @@ class DxNameTest extends AnyFlatSpec with Matchers {
   }
 
   it should "encode and decode WDL names" in {
-    WdlDxName.fromDecodedName("a.b.c").encoded shouldBe "a___b___c"
-    WdlDxName.fromEncodedName("a___b___c").decoded shouldBe "a.b.c"
+    WdlDxName.fromDecodedName("a").encoded shouldBe "__46__a"
+    WdlDxName.fromDecodedName("a.b").encoded shouldBe "__46__a__46__b"
+    WdlDxName.fromDecodedName("a.b___dxfiles").encoded shouldBe "__46__a__46__b___dxfiles"
 
-    WdlDxName.fromDecodedName("_foo.bar_").encoded shouldBe "_foo___bar_"
-    WdlDxName.fromEncodedName("_foo___bar_").decoded shouldBe "_foo.bar_"
+    WdlDxName.fromDecodedName("46.a.b.c").encoded shouldBe "__46__46__46__a__46__b__46__c"
+    WdlDxName.fromEncodedName("__46__46__46__a__46__b__46__c").decoded shouldBe "46.a.b.c"
 
-    WdlDxName.fromDecodedName("foo.bar___").encoded shouldBe "foo___bar___"
-    WdlDxName.fromEncodedName("foo___bar___").decoded shouldBe "foo.bar___"
+    WdlDxName.fromDecodedName("_foo.bar_").encoded shouldBe "__46___foo__46__bar_"
+    WdlDxName.fromEncodedName("__46___foo__46__bar_").decoded shouldBe "_foo.bar_"
 
-    WdlDxName.fromDecodedName("stage-1.foo.bar___").encoded shouldBe "stage-1.foo___bar___"
-    WdlDxName.fromEncodedName("stage-1.foo___bar___").decoded shouldBe "stage-1.foo.bar___"
+    WdlDxName.fromDecodedName("foo.bar___").encoded shouldBe "__46__foo__46__bar___"
+    WdlDxName.fromEncodedName("foo__46__bar___").decoded shouldBe "foo.bar___"
+    WdlDxName.fromEncodedName("__46__foo__46__bar___").decoded shouldBe "foo.bar___"
+
+    WdlDxName.fromDecodedName("stage-1.foo.bar___").encoded shouldBe "stage-1.__46__foo__46__bar___"
+    WdlDxName.fromEncodedName("stage-1.foo__46__bar___").decoded shouldBe "stage-1.foo.bar___"
+    WdlDxName.fromEncodedName("stage-1.__46__foo__46__bar___").decoded shouldBe "stage-1.foo.bar___"
   }
 
   it should "encode a valid WDL name" in {
-    val dxName = WdlDxName.fromDecodedName("stage-1.a.b___dxfiles")
+    val dxName = WdlDxName.fromDecodedName("stage-1.46.b___dxfiles")
     dxName.numParts shouldBe 2
-    dxName.getDecodedParts shouldBe Vector("a", "b")
-    dxName.getEncodedParts shouldBe Vector("a", "b")
-    dxName.decoded shouldBe "stage-1.a.b___dxfiles"
-    dxName.encoded shouldBe "stage-1.a___b___dxfiles"
+    dxName.getDecodedParts shouldBe Vector("46", "b")
+    dxName.getEncodedParts shouldBe Vector("46", "b")
+    dxName.decoded shouldBe "stage-1.46.b___dxfiles"
+    dxName.encoded shouldBe "stage-1.__46__46__46__b___dxfiles"
     dxName.stage shouldBe Some("stage-1")
     dxName.suffix shouldBe Some("___dxfiles")
 
@@ -56,44 +62,44 @@ class DxNameTest extends AnyFlatSpec with Matchers {
     dxName.endsWith(dxName2) shouldBe true
 
     val dxName3 = WdlDxName.fromDecodedName("stage-1.b___dxfiles")
-    dxName3.pushDecodedNamespace("a") shouldBe dxName
-    dxName.popDecodedIdentifier() shouldBe (WdlDxName.fromDecodedName("stage-1.a"), "b")
+    dxName3.pushDecodedNamespace("46") shouldBe dxName
+    dxName.popDecodedIdentifier() shouldBe (WdlDxName.fromDecodedName("stage-1.46"), "b")
     dxName.popDecodedIdentifier(keepSuffix = true) shouldBe (
-        WdlDxName.fromDecodedName("stage-1.a___dxfiles"), "b"
+        WdlDxName.fromDecodedName("stage-1.46___dxfiles"), "b"
     )
 
     val dxName4 = dxName.dropSuffix()
     dxName4.suffix shouldBe None
-    dxName4.encoded shouldBe "stage-1.a___b"
-    dxName4.decoded shouldBe "stage-1.a.b"
+    dxName4.encoded shouldBe "stage-1.__46__46__46__b"
+    dxName4.decoded shouldBe "stage-1.46.b"
 
     dxName4.withSuffix("___dxfiles") shouldBe dxName
   }
 
   it should "decode valid WDL names" in {
-    val dxName = WdlDxName.fromEncodedName("a___b___dxfiles")
+    val dxName = WdlDxName.fromEncodedName("__46__a__46__b___dxfiles")
     dxName.numParts shouldBe 2
     dxName.getDecodedParts shouldBe Vector("a", "b")
     dxName.getEncodedParts shouldBe Vector("a", "b")
     dxName.decoded shouldBe "a.b___dxfiles"
-    dxName.encoded shouldBe "a___b___dxfiles"
+    dxName.encoded shouldBe "__46__a__46__b___dxfiles"
     dxName.suffix shouldBe Some("___dxfiles")
 
-    val dxName2 = WdlDxName.fromEncodedName("b___dxfiles")
+    val dxName2 = WdlDxName.fromEncodedName("__46__b___dxfiles")
     dxName.endsWith(dxName2) shouldBe true
     dxName2.pushDecodedNamespace("a") shouldBe dxName
     dxName.popDecodedIdentifier() shouldBe (WdlDxName.fromEncodedName("a"), "b")
 
     val dxName3 = dxName.dropSuffix()
     dxName3.suffix shouldBe None
-    dxName3.encoded shouldBe "a___b"
+    dxName3.encoded shouldBe "__46__a__46__b"
     dxName3.decoded shouldBe "a.b"
 
     dxName3.withSuffix("___dxfiles") shouldBe dxName
   }
 
   it should "decode a WDL name with multiple stages" in {
-    val dxName = WdlDxName.fromEncodedName("stage-14.stage-22.a___dxfiles")
+    val dxName = WdlDxName.fromEncodedName("stage-14.stage-22.__46__a___dxfiles")
     dxName.stage shouldBe Some("stage-14.stage-22")
     dxName.getDecodedParts shouldBe Vector("a")
     dxName.suffix shouldBe Some("___dxfiles")
@@ -109,9 +115,9 @@ class DxNameTest extends AnyFlatSpec with Matchers {
     assertThrows[Throwable] {
       WdlDxName.fromDecodedName("foo.")
     }
-    assertThrows[Throwable] {
-      WdlDxName.fromDecodedName("a_._b")
-    }
+    // assertThrows[Throwable] {
+    //   WdlDxName.fromDecodedName("a_._b")
+    // }
     assertThrows[Throwable] {
       WdlDxName.fromDecodedName("a-b.c")
     }
@@ -142,25 +148,31 @@ class DxNameTest extends AnyFlatSpec with Matchers {
   }
 
   it should "hash and compare two WDL names" in {
-    val dxName1 = WdlDxName.fromEncodedName("a___b___dxfiles")
+    val dxName1 = WdlDxName.fromEncodedName("a__46__b___dxfiles")
+    val dxName1alter = WdlDxName.fromEncodedName("a__46__b___dxfiles")
     val dxName2 = WdlDxName.fromDecodedName("a.b___dxfiles")
     (dxName1 == dxName2) shouldBe true
+    (dxName1alter == dxName2) shouldBe true
     val m = Map(dxName1 -> "x")
     m(dxName2) shouldBe "x"
   }
 
   it should "encode and decode CWL names" in {
-    CwlDxName.fromDecodedName("a/b/c").encoded shouldBe "a___b___c"
-    CwlDxName.fromEncodedName("a___b___c").decoded shouldBe "a/b/c"
+    CwlDxName.fromDecodedName("a/b/c").encoded shouldBe "__47__a__47__b__47__c"
+    CwlDxName.fromEncodedName("a__47__b__47__c").decoded shouldBe "a/b/c"
+    CwlDxName.fromEncodedName("__47__a__47__b__47__c").decoded shouldBe "a/b/c"
 
-    CwlDxName.fromDecodedName("_foo/bar_").encoded shouldBe "_foo___bar_"
-    CwlDxName.fromEncodedName("_foo___bar_").decoded shouldBe "_foo/bar_"
+    CwlDxName.fromDecodedName("_foo/bar_").encoded shouldBe "__47___foo__47__bar_"
+    CwlDxName.fromEncodedName("_foo__47__bar_").decoded shouldBe "_foo/bar_"
+    CwlDxName.fromEncodedName("__47___foo__47__bar_").decoded shouldBe "_foo/bar_"
 
-    CwlDxName.fromDecodedName("a-9/c.d").encoded shouldBe "a__45__9___c__46__d"
-    CwlDxName.fromEncodedName("a__45__9___c__46__d").decoded shouldBe "a-9/c.d"
+    CwlDxName.fromDecodedName("a-9/c.d").encoded shouldBe "__47__a__45__9__47__c__46__d"
+    CwlDxName.fromEncodedName("a__45__9__47__c__46__d").decoded shouldBe "a-9/c.d"
+    CwlDxName.fromEncodedName("__47__a__45__9__47__c__46__d").decoded shouldBe "a-9/c.d"
 
-    CwlDxName.fromDecodedName("foo/bar___").encoded shouldBe "foo___bar___"
-    CwlDxName.fromEncodedName("foo___bar___").decoded shouldBe "foo/bar___"
+    CwlDxName.fromDecodedName("foo/bar___").encoded shouldBe "__47__foo__47__bar___"
+    CwlDxName.fromEncodedName("foo__47__bar___").decoded shouldBe "foo/bar___"
+    CwlDxName.fromEncodedName("__47__foo__47__bar___").decoded shouldBe "foo/bar___"
   }
 
   it should "encode a valid CWL name" in {
@@ -169,7 +181,7 @@ class DxNameTest extends AnyFlatSpec with Matchers {
     dxName.getDecodedParts shouldBe Vector("a-b", "c.d")
     dxName.getEncodedParts shouldBe Vector("a__45__b", "c__46__d")
     dxName.decoded shouldBe "a-b/c.d___dxfiles"
-    dxName.encoded shouldBe "a__45__b___c__46__d___dxfiles"
+    dxName.encoded shouldBe "__47__a__45__b__47__c__46__d___dxfiles"
     dxName.suffix shouldBe Some("___dxfiles")
 
     val dxName2 = CwlDxName.fromDecodedName("c.d___dxfiles")
@@ -179,19 +191,19 @@ class DxNameTest extends AnyFlatSpec with Matchers {
 
     val dxName3 = dxName.dropSuffix()
     dxName3.suffix shouldBe None
-    dxName3.encoded shouldBe "a__45__b___c__46__d"
+    dxName3.encoded shouldBe "__47__a__45__b__47__c__46__d"
     dxName3.decoded shouldBe "a-b/c.d"
 
     dxName3.withSuffix("___dxfiles") shouldBe dxName
   }
 
   it should "decode a valid CWL name" in {
-    val dxName = CwlDxName.fromEncodedName("a__45__b___c__46__d___dxfiles")
+    val dxName = CwlDxName.fromEncodedName("a__45__b__47__c__46__d___dxfiles")
     dxName.numParts shouldBe 2
     dxName.getDecodedParts shouldBe Vector("a-b", "c.d")
     dxName.getEncodedParts shouldBe Vector("a__45__b", "c__46__d")
     dxName.decoded shouldBe "a-b/c.d___dxfiles"
-    dxName.encoded shouldBe "a__45__b___c__46__d___dxfiles"
+    dxName.encoded shouldBe "__47__a__45__b__47__c__46__d___dxfiles"
     dxName.suffix shouldBe Some("___dxfiles")
 
     val dxName2 = CwlDxName.fromEncodedName("c__46__d___dxfiles")
@@ -201,7 +213,7 @@ class DxNameTest extends AnyFlatSpec with Matchers {
 
     val dxName3 = dxName.dropSuffix()
     dxName3.suffix shouldBe None
-    dxName3.encoded shouldBe "a__45__b___c__46__d"
+    dxName3.encoded shouldBe "__47__a__45__b__47__c__46__d"
     dxName3.decoded shouldBe "a-b/c.d"
 
     dxName3.withSuffix("___dxfiles") shouldBe dxName
@@ -211,18 +223,18 @@ class DxNameTest extends AnyFlatSpec with Matchers {
     assertThrows[Throwable] {
       CwlDxName.fromDecodedName("  ")
     }
-    assertThrows[Throwable] {
-      CwlDxName.fromDecodedName("a__b")
-    }
+    // assertThrows[Throwable] {
+    //   CwlDxName.fromDecodedName("a__b")
+    // }
     assertThrows[Throwable] {
       CwlDxName.fromDecodedName("a b")
     }
     assertThrows[Throwable] {
       CwlDxName.fromDecodedName("foo/")
     }
-    assertThrows[Throwable] {
-      CwlDxName.fromDecodedName("a_/_b")
-    }
+    // assertThrows[Throwable] {
+    //   CwlDxName.fromDecodedName("a_/_b")
+    // }
     assertThrows[Throwable] {
       CwlDxName.fromSourceName("a/b")
     }
@@ -255,9 +267,11 @@ class DxNameTest extends AnyFlatSpec with Matchers {
   }
 
   it should "hash and compare two CWL names" in {
-    val dxName1 = CwlDxName.fromEncodedName("a___b___dxfiles")
+    val dxName1 = CwlDxName.fromEncodedName("a__47__b___dxfiles")
+    val dxName1alter = CwlDxName.fromEncodedName("__47__a__47__b___dxfiles")
     val dxName2 = CwlDxName.fromDecodedName("a/b___dxfiles")
     (dxName1 == dxName2) shouldBe true
+    (dxName1alter == dxName2) shouldBe true
     val m = Map(dxName1 -> "x")
     m(dxName2) shouldBe "x"
   }
