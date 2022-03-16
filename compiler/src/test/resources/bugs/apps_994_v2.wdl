6b995b8ff2b16f632dfd64cde68f7bf034ecf356
Gvaihir <pcf11differentiation@gmail.com>
2022-03-15 17:21:46 -0700

    APPS-994 Update reuse applet from task (feat) CallableTranslatorTest.scala - tests passing

---
+++ compiler/src/test/resources/bugs/apps_994_v2.wdl
@@ -0,0 +1,93 @@
+version 1.1
+
+task reuse_multiply {
+	input {
+		Int first
+		Int second
+	}
+
+	command <<< >>>
+
+	runtime {
+		container: "ubuntu:20.04"
+	}
+
+	Int product = first * second
+
+	output {
+		Int out = product
+	}
+}
+
+task reuse_print {
+	input {
+		String str
+	}
+
+	command <<<
+	set -euxo pipefail
+	echo ~{str} > reuse_print.txt
+	>>>
+
+	runtime {
+		container: "ubuntu:20.04"
+	}
+
+	output {
+		File out = "reuse_print.txt"
+	}
+}
+
+workflow reuse {
+
+	# Frag applet via sub-expression
+	# declaration of inputs is dofferent from V1. Therefore this frag should be rebuilt
+	Int f = 1 + 10000
+	Int s = 4 + 5
+	call reuse_multiply as sub_expr {
+		input: first = f, second = s
+	}
+
+	# Frag applet via conditional, single call
+	Boolean t = true
+	if (t) {
+		call reuse_multiply as conditional_single {
+			input: first = 1, second = 2
+		}
+	}
+
+	# Frag applet via conditional, multiple call
+	if (!t) {
+		call reuse_multiply as conditional_multi_1 {
+			input: first = 3, second = 4
+		}
+		call reuse_multiply as conditional_multi_2 {
+			input: first = 5, second = 6
+		}
+	}
+
+	# Frag applet via scatter, single call
+	Array[String] scatter_single_arr = ["Hello", "World"]
+	scatter (i in scatter_single_arr) {
+		call reuse_print as scatter_single {
+			input: str = i
+		}
+	}
+
+	# Frag applet via scatter, multiple call
+	Array[String] scatter_multi_arr = ["Hello", "Good day", "Ahoj", "Dobry den"]
+	String punctuation = "!"
+	scatter (i in scatter_multi_arr) {
+		call reuse_print as scatter_multi_1 {
+			input: str = i
+		}
+		call reuse_print as scatter_multi_2 {
+			input: str = punctuation
+		}
+	}
+
+	# Do minor edit unrelated to frag applets
+	call reuse_print as ZZZ {
+		input: str = "Unrelated"
+	}
+}
--- compiler/src/test/scala/dx/translator/wdl/CallableTranslatorTest.scala
+++ compiler/src/test/scala/dx/translator/wdl/CallableTranslatorTest.scala
@@ -3,10 +3,10 @@ package dx.translator.wdl
 import dx.core.ir.{Application, Callable, InstanceTypeSelection, SourceCode, Workflow}
 import dx.core.languages.wdl.{VersionSupport, WdlBundle}
 import dx.translator.DefaultReorgSettings
-//import dx.util.CodecUtils
+import dx.util.Bindings
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import wdlTools.types.{TypedAbstractSyntax => TAT}
+import wdlTools.types.{WdlTypes, TypedAbstractSyntax => TAT}
 
 import java.nio.file.{Path, Paths}
 
@@ -21,13 +21,13 @@ class CallableTranslatorTest extends AnyFlatSpec with Matchers {
     case _  => srcDoc.getDocContents
   }
 
-  "CallableTranslator" should "render different wdl code for every block/app/frag" in {
-    val (docV1, typeAliasesV1, versionSupportV1) =
-      VersionSupport.fromSourceFile(pathFromBasename("bugs", "apps_994_v1.wdl"))
-    val wdlBundleV1: WdlBundle = WdlBundle.create(doc = docV1)
+  private def getDeconstructedCallables(doc: TAT.Document,
+                                        typeAliases: Bindings[String, WdlTypes.T_Struct],
+                                        versionSupport: VersionSupport): Map[String, String] = {
+    val wdlBundleV1: WdlBundle = WdlBundle.create(doc = doc)
     val callableTranslatorV1: CallableTranslator = CallableTranslator(
         wdlBundle = wdlBundleV1,
-        typeAliases = typeAliasesV1.toMap,
+        typeAliases = typeAliases.toMap,
         locked = false,
         defaultRuntimeAttrs = Map.empty,
         reorgAttrs = DefaultReorgSettings(false),
@@ -35,7 +35,7 @@ class CallableTranslatorTest extends AnyFlatSpec with Matchers {
         defaultScatterChunkSize = 1,
         useManifests = false,
         instanceTypeSelection = InstanceTypeSelection.Static,
-        versionSupport = versionSupportV1
+        versionSupport = versionSupport
     )
     val depOrder: Vector[TAT.Callable] = wdlBundleV1.sortByDependencies()
     val (_, sortedCallables) =
@@ -47,12 +47,22 @@ class CallableTranslatorTest extends AnyFlatSpec with Matchers {
               sortedCallables ++ translatedCallables
           )
       }
-    val deconstructedCallables: Map[String, String] = sortedCallables.map {
+    sortedCallables.map {
       case Application(name, _, _, _, _, _, document, _, _, _, _, _) =>
         name -> docOrContents(document)
       case Workflow(name, _, _, _, document, _, _, _, _, _, _) =>
         name -> docOrContents(document)
     }.toMap
+  }
+
+  "CallableTranslator" should "render different wdl code for every block/app/frag" in {
+    val (doc, typeAliases, versionSupport) =
+      VersionSupport.fromSourceFile(pathFromBasename("bugs", "apps_994_v1.wdl"))
+    val deconstructedCallables = getDeconstructedCallables(
+        doc = doc,
+        typeAliases = typeAliases,
+        versionSupport = versionSupport
+    )
     deconstructedCallables("reuse_print") should not equal deconstructedCallables("reuse_multiply")
     deconstructedCallables("reuse_block_2") should not equal deconstructedCallables("reuse_block_4")
     deconstructedCallables("reuse_frag_stage-12") should not equal deconstructedCallables(
@@ -61,5 +71,35 @@ class CallableTranslatorTest extends AnyFlatSpec with Matchers {
     deconstructedCallables("reuse_frag_stage-0") should not equal deconstructedCallables(
         "reuse_block_4"
     )
+  }
+
+  it should "render same wdl code for every unchanged block/app/frag and different if there are changes" in {
+    val (docV1, typeAliasesV1, versionSupportV1) =
+      VersionSupport.fromSourceFile(pathFromBasename("bugs", "apps_994_v1.wdl"))
+    val (docV2, typeAliasesV2, versionSupportV2) =
+      VersionSupport.fromSourceFile(pathFromBasename("bugs", "apps_994_v2.wdl"))
+    val deconstructedCallablesV1 = getDeconstructedCallables(
+        doc = docV1,
+        typeAliases = typeAliasesV1,
+        versionSupport = versionSupportV1
+    )
+    val deconstructedCallablesV2 = getDeconstructedCallables(
+        doc = docV2,
+        typeAliases = typeAliasesV2,
+        versionSupport = versionSupportV2
+    )
+
+    deconstructedCallablesV1("reuse_print") should equal(deconstructedCallablesV2("reuse_print"))
+    deconstructedCallablesV1("reuse_block_2") should equal(
+        deconstructedCallablesV2("reuse_block_2")
+    )
+    deconstructedCallablesV1("reuse_frag_stage-12") should equal(
+        deconstructedCallablesV2(
+            "reuse_frag_stage-12"
+        )
+    )
+    deconstructedCallablesV1("reuse_frag_stage-0") should not equal (deconstructedCallablesV2(
+        "reuse_frag_stage-0"
+    ))
   }
 }
