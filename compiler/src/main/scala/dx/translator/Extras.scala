8aa3bd859c3c13001f3fd8d6d2bc18c3123cfd9f
Marek Hrvol <mhrvol-cf@dnanexus.com>
2022-04-06 14:30:26 +0200

    debug

--- compiler/src/main/scala/dx/translator/Extras.scala
+++ compiler/src/main/scala/dx/translator/Extras.scala
@@ -13,7 +13,8 @@ import spray.json._
 object ExtrasJsonProtocol extends DefaultJsonProtocol {
   implicit object DxAccessLevelProtocol extends RootJsonFormat[DxAccessLevel] {
     def write(level: DxAccessLevel): JsString = JsString(level.name)
-    def read(jsv: JsValue, logger: Logger = Logger.get): DxAccessLevel = {
+    def read(jsv: JsValue): DxAccessLevel = {
+      logger = Logger.get
       logger.warning(jsv.toString)
       jsv match {
         case JsString(name) => DxAccessLevel.withName(name)
