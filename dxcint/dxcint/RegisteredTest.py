528f844a23ac94f52fa5a797586fe76a581f7186
Gvaihir <pcf11differentiation@gmail.com>
2022-06-22 16:02:44 -0700

    APPS-1121 Update integration test suite (feat) RegisteredTest.py - implemented base class

--- dxcint/dxcint/RegisteredTest.py
+++ dxcint/dxcint/RegisteredTest.py
@@ -2,6 +2,8 @@ import logging
 import os
 import shlex
 import random
+import dxpy
+
 import subprocess as sp
 from typing import List, Optional
 
@@ -32,6 +34,14 @@ class RegisteredTest(object):
         self._messenger = None
         self._exec_id = None
         self._job_id = None
+        self._executable_type_switch = {
+            "workflow": dxpy.DXWorkflow,
+            "app": dxpy.DXApp,
+            "applet": dxpy.DXApplet
+        }
+        self._git_revision = sp.check_output(
+            ["git", "describe", "--always", "--dirty", "--tags"]
+        ).strip()
 
     @property
     def category(self):
@@ -61,33 +71,53 @@ class RegisteredTest(object):
     def job_id(self):
         if not self._job_id:
             self._job_id = self._run_executable()
-            # TODO STOPPED HERE
+        return self._job_id
 
     def _compile_executable(self, additional_compiler_flags: Optional[List[str]] = None) -> str:
+        """
+        Basic implementation. For different test classes override `exec_id` property with calling this method with
+        arguments which suite particular test type. For example, when implementing class ManifestTest(RegisteredTest)
+        call this method with `additional_compiler_flags=['-useManifests']` argument in parameter `exec_id`.
+        Args:
+            additional_compiler_flags: Optional[List[str]]. Use this argument to alter the compiler behavior for
+            concrete class implementation
+
+        Returns: str.
+        """
         compiler_flags = ["-instanceTypeSelection", random.choice(["static", "dynamic"])]
-        if "manifest" in self._src_file:
-            compiler_flags.append("-useManifests")
         compiler_flags += additional_compiler_flags or []
         compiler_jar_path = os.path.join(self._context.repo_root_dir, f"dxCompiler-{self._context.version}.jar")
         compile_dir = os.path.join(self._context.platform_build_dir, "applets", self._test_name)
-        cmd = f"java -jar {compiler_jar_path} compile {self._src_file} -force -folder {compile_dir} -project " \
-                  f"{self._context.project_id}"
+        cmd = f"java -jar {compiler_jar_path} compile {self._src_file} -force -folder {compile_dir} " \
+              f"-project {self._context.project_id}"
         cmd += " ".join(compiler_flags)
         try:
             logging.info(f"COMPILE COMMAND: {cmd}")
-            oid = sp.check_output(shlex.split(cmd)).strip()
-        except sp.CalledProcessError as cpe:
+            workflow_id = sp.check_output(shlex.split(cmd)).strip()
+        except sp.CalledProcessError as e:
             logging.error(
                 f"Error compiling {self._src_file}\n"
-                f"stdout: {cpe.stdout}\n"
-                f"stderr: {cpe.stderr}"
+                f"stdout: {e.stdout}\n"
+                f"stderr: {e.stderr}"
             )
-            raise cpe
-        return oid.decode("ascii")
+            raise e
+        return workflow_id.decode("ascii")
+
+    def _run_executable(self):
+        exec_type = self.exec_id.split("-")[0]
+        exec_input = {}
+        exec_handler = self._executable_type_switch.get(exec_type)(
+            project=self._context.project_id,
+            dxid=self.exec_id
+        )
+        dx_execution = exec_handler.run(
+            exec_input,
+            project=self._context.project_id,
+            folder=os.path.join(self._context.platform_build_dir, "test"),
+            name="{} {}".format(self._test_name, self._git_revision)
+        )
+        exec_desc = dx_execution.describe()
+        return exec_desc.get("id")
 
     def _create_messenger(self) -> Messenger:
-        return Messenger(
-            test_name=self.name,
-            job_id=self.job_id,
-            variant=None  # TODO
-        )
+        raise RegisteredTestError("Messenger is not implemented")
