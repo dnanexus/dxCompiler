9bbaa25c0f8f2a08ce82baba31e2e37aab2129c6
Yuxin Shi <yshi@dnanexus.com>
2022-07-19 18:42:10 -0700

    fix casting optional types between scatters

--- executorCwl/src/main/scala/dx/executor/cwl/CwlWorkflowExecutor.scala
+++ executorCwl/src/main/scala/dx/executor/cwl/CwlWorkflowExecutor.scala
@@ -192,15 +192,15 @@ case class CwlWorkflowExecutor(workflow: Workflow,
       case path =>
         val block: CwlBlock = Block.getSubBlockAt(CwlBlock.createBlocks(workflow), path)
         val inputTypes = block.inputs.map {
-          case RequiredBlockInput(name, param: Parameter with Loadable) =>
-            name -> (CwlUtils.toIRType(param.cwlType), param.loadContents, param.loadListing)
-          case RequiredBlockInput(name, param) =>
-            name -> (CwlUtils.toIRType(param.cwlType), false, LoadListing.No)
-          case OptionalBlockInput(name, param: Parameter with Loadable) =>
-            val irType = Type.ensureOptional(CwlUtils.toIRType(param.cwlType))
+          case RequiredBlockInput(name, cwlType, param: Parameter with Loadable) =>
+            name -> (CwlUtils.toIRType(cwlType), param.loadContents, param.loadListing)
+          case RequiredBlockInput(name, cwlType, param) =>
+            name -> (CwlUtils.toIRType(cwlType), false, LoadListing.No)
+          case OptionalBlockInput(name, cwlType, param: Parameter with Loadable) =>
+            val irType = Type.ensureOptional(CwlUtils.toIRType(cwlType))
             name -> (irType, param.loadContents, param.loadListing)
-          case OptionalBlockInput(name, param) =>
-            name -> (Type.ensureOptional(CwlUtils.toIRType(param.cwlType)), false, LoadListing.No)
+          case OptionalBlockInput(name, cwlType, param) =>
+            name -> (Type.ensureOptional(CwlUtils.toIRType(cwlType)), false, LoadListing.No)
         }.toMap
         if (logger.isVerbose) {
           logger.trace(
@@ -697,6 +697,9 @@ case class CwlWorkflowExecutor(workflow: Workflow,
           case Some((_, (_, ArrayValue(items)))) if items.isEmpty     => None
           case Some((_, (array: CwlArray, ArrayValue(items)))) =>
             Some(array.itemType, items)
+          case Some((_, (t, ArrayValue(items))))
+              if CwlOptional.unwrapOptional(t).isInstanceOf[CwlArray] =>
+            Some(CwlOptional.unwrapOptional(t), items)
           case None =>
             throw new Exception(s"scatter parameter ${name} is missing from step inputs")
           case Some((_, (t, _))) =>
@@ -869,8 +872,8 @@ case class CwlWorkflowExecutor(workflow: Workflow,
                                                    inp.cwlType,
                                                    inp.name.decoded,
                                                    isInput = true)
-        case (OptionalBlockInput(name, param), None) =>
-          name -> (param.cwlType, NullValue)
+        case (OptionalBlockInput(name, cwlType, param), None) =>
+          name -> (cwlType, NullValue)
         case _ => throw new Exception(s"missing required input ${inp.name}")
       }
     }.toMap
