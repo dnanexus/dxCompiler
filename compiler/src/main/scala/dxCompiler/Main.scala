239540d658c47272fcf6f5aa7d7b66a348e0a193
emiloslavsky <emiloslavsky@users.noreply.github.com>
2022-08-26 14:04:18 -0700

    APPS-1351 dxni help string: support apps and applets only (#376)

--- compiler/src/main/scala/dxCompiler/Main.scala
+++ compiler/src/main/scala/dxCompiler/Main.scala
@@ -888,8 +888,8 @@ object Main {
         |      -waitOnUpload          Whether to wait for each file upload to complete.
         |
         |  dxni (WDL only)
-        |    DNAnexus Native call Interface. Creates stubs for calling DNAnexus executables 
-        |    (apps/applets/workflows), and stores them as WDL tasks in a local file. Enables 
+        |    DNAnexus Native call Interface. Creates stubs for calling DNAnexus 
+        |    apps/applets, and stores them as WDL tasks in a local file. Enables 
         |    calling existing platform executables without modification.
         |    options:
         |      -apps [include,exclude,only]
