0627dd17230704b7db8b5365e627cb5178e9053a
Jan Provazník <jprovaznik@dnanexus.com>
2022-09-02 20:28:04 +0200

    APPS-1366 dxcint test GHA
    * dxcint 
    (feat) GHA for testing dxcint
    
    Co-authored-by: Gvaihir <pcf11differentiation@gmail.com>

--- dxcint/tests/test_RegisteredTest.py
+++ dxcint/tests/test_RegisteredTest.py
@@ -32,7 +32,7 @@ def test_init(registered_test_wdl):
     assert registered_test_wdl.category == "mock_category"
     assert registered_test_wdl.name == "mock_1"
     assert registered_test_wdl.language == "wdl"
-    assert registered_test_wdl._test_inputs == {"in_1": "Hello World!"}
+    assert registered_test_wdl._test_inputs == {"stage-common.in_1": "Hello World!"}
 
 
 def test_build_executable_wdl(build_executable_wdl):
