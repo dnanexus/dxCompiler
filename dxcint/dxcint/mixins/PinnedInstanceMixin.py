d669f87119e9f02ab88572f5c72ebdbabe51bb05
Gvaihir <pcf11differentiation@gmail.com>
2023-04-04 00:56:42 -0700

    APPS-1377 migrate moderate tests to dxcint (feat) PinnedInstanceMixin.py - tests (fix) elsewhere - refactor

---
+++ dxcint/dxcint/mixins/PinnedInstanceMixin.py
@@ -0,0 +1,12 @@
+from dxcint.RegisteredTest import RegisteredTest
+
+
+class PinnedInstanceMixin(RegisteredTest):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def job_id(self) -> str:
+        if not self._job_id:
+            self._job_id = self._run_executable(**{"instance_type": None})
+        return self._job_id
