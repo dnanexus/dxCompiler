75029021c7e63d37ed017019e6af32623e305037
Gvaihir <pcf11differentiation@gmail.com>
2023-04-04 00:39:21 -0700

    APPS-1377 migrate moderate tests to dxcint (feat) StaticOnlyMixin.py - tests (fix) elsewhere - refactor

--- dxcint/dxcint/mixins/StaticOnlyMixin.py
+++ dxcint/dxcint/mixins/StaticOnlyMixin.py
@@ -5,11 +5,10 @@ class StaticOnlyMixin(RegisteredTest):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def _compile_executable(self, *args, **kwargs) -> str:
-        kwargs["additional_compiler_flags"] = kwargs.get(
-            "additional_compiler_flags", []
-        ) + [
-            "-instanceTypeSelection",
-            "static",
-        ]
-        return super()._compile_executable(*args, **kwargs)
+    @property
+    def exec_id(self) -> str:
+        if not self._exec_id:
+            self._exec_id = self._compile_executable(
+                {"-instanceTypeSelection": "static"}
+            )
+        return self._exec_id
