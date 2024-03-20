10628ea4325afc8c7a607ebf2678f97212100d0c
Riva Kepych <rkepych@dnanexus.com>
2024-03-20 13:34:07 +0100

    run script

--- _scratchpad_/run.sh
+++ _scratchpad_/run.sh
@@ -2,6 +2,10 @@
 
 PROJECT=project-Fy9QqgQ0yzZbg9KXKP4Jz6Yq
 FOLDER=/rkepych
-DXCOMPILER_JAR=~/Documents/dxCompiler/applet_resources/dxCompiler.jar
+DXCOMPILER_JAR=../dxCompiler-2.11.6-SNAPSHOT.jar
 
-java -jar $DXCOMPILER_JAR compile prepare_scatter_gather.wdl -project $PROJECT -folder $FOLDER
+RUN_OPTS='--ignore-reuse --destination ${PROJECT}:${FOLDER} --preserve-job-'
+
+workflow=$(java -jar $DXCOMPILER_JAR compile prepare_scatter_gather.wdl -project $PROJECT -folder $FOLDER -force)
+
+dx run $workflow $RUN_OPTS
