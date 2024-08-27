4e9c34377919480073c2f9c42311af009d841e32
Riva Kepych <rkepych@dnanexus.com>
2024-08-27 23:47:13 +0200

    docker test script wip

---
+++ scripts/docker_image/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Find the root directory of the distribution, regardless of where
+# the script is called from
+base_dir=$(dirname "$0")
+source "${base_dir}/docker.env"
+
+if [[ -z "${VERSION}" ]]; then
+  echo "Specify the dxCompiler version in docker.env"
+  exit 1
+fi
+
+bash "${base_dir}/docker_build.sh" 2>&1
+
+bash "${base_dir}/docker_run.sh" version 2>&1
