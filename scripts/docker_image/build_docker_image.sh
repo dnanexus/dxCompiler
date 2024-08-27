a7537a97ced57888cfdb71fee37d68bc4a00ac97
Riva Kepych <rkepych@dnanexus.com>
2024-08-27 17:30:10 +0200

    build and tag image

--- scripts/docker_image/build_docker_image.sh
+++ scripts/docker_image/build_docker_image.sh
@@ -5,7 +5,7 @@
 VERSION=2.11.6
 
 if [[ -z "${VERSION}" ]]; then
-  echo "Specify the dxCompiler version in the script"
+  echo "Specify VERSION = the dxCompiler version to use"
   exit 1
 fi
 
@@ -13,8 +13,6 @@ echo "Downloading dxCompiler v${VERSION} from GitHub"
 rm -f dxCompiler-${VERSION}.jar
 wget https://github.com/dnanexus/dxCompiler/releases/download/${VERSION}/dxCompiler-${VERSION}.jar
 
-# echo "building a docker image"
-# sudo docker build --build-arg VERSION=${version} -t dnanexus/dxcompiler:${version} .
-
-# echo "tagging as latest"
-# sudo docker tag dnanexus/dxcompiler:${version} dnanexus/dxcompiler:latest
+echo "Building Docker image"
+docker build --build-arg VERSION=${VERSION} -t dnanexus/dxcompiler:${VERSION} .
+docker tag dnanexus/dxcompiler:${VERSION} dnanexus/dxcompiler:latest
