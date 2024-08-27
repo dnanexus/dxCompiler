6a9b5971858e619dd92d6f47586046f742ce44b3
Riva Kepych <rkepych@dnanexus.com>
2024-08-27 17:25:41 +0200

    download dxCompiler from GH releases

--- scripts/docker_image/build_docker_image.sh
+++ scripts/docker_image/build_docker_image.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -e
 
-# TODO Specify the version
+# Specify the dxCompiler version from
+# https://github.com/dnanexus/dxCompiler/releases
+VERSION=2.11.6
 
-# TODO Download dxCompiler-${version}.jar
-
-if [[ ! -f dxCompiler-${version}.jar ]]; then
-    # To build the docker image, we need a copy of the jar file. We
-    # download it from github to make sure we have an up to date version.
-    rm -f dxCompiler-${version}.jar
-    wget https://github.com/dnanexus/dxCompiler/releases/download/${version}/dxCompiler-${version}.jar
+if [[ -z "${VERSION}" ]]; then
+  echo "Specify the dxCompiler version in the script"
+  exit 1
 fi
 
-echo "building a docker image"
-sudo docker build --build-arg VERSION=${version} -t dnanexus/dxcompiler:${version} .
+echo "Downloading dxCompiler v${VERSION} from GitHub"
+rm -f dxCompiler-${VERSION}.jar
+wget https://github.com/dnanexus/dxCompiler/releases/download/${VERSION}/dxCompiler-${VERSION}.jar
 
-echo "tagging as latest"
-sudo docker tag dnanexus/dxcompiler:${version} dnanexus/dxcompiler:latest
+# echo "building a docker image"
+# sudo docker build --build-arg VERSION=${version} -t dnanexus/dxcompiler:${version} .
+
+# echo "tagging as latest"
+# sudo docker tag dnanexus/dxcompiler:${version} dnanexus/dxcompiler:latest
