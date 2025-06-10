791e1ed97224152d4e869358ac5e85e3eb111fc8
Ted Brookings <ted@fulcrumgenomics.com>
2025-06-10 15:22:43 -0400

    Make load of application.conf synchronzed just to be safe

--- core/src/main/scala/dx/core/package.scala
+++ core/src/main/scala/dx/core/package.scala
@@ -5,7 +5,9 @@ import com.typesafe.config.ConfigFactory
 package object core {
   // The version lives in application.conf
   def getVersion: String = {
-    val config = ConfigFactory.load("application.conf")
+    val config = this.synchronized {
+      ConfigFactory.load("application.conf")
+    }
     config.getString("dxCompilerCore.version")
   }
 }
